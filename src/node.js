c4f1eb237c25b69ea0d0e258f98eab714c5de4ec
svyatlo <svyatlo@list.ru>
2019-02-21 12:32:20 +0300

    fill constructors of classes

--- src/node.js
+++ src/node.js
@@ -1,6 +1,10 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,8 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
